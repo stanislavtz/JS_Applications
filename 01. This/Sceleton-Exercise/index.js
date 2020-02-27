461393216beb8f16949c0aaf4775b9e80764cbc0
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-02-28 00:42:56 +0200

    Table solution upload

--- 01. This/Sceleton-Exercise/index.js
+++ 01. This/Sceleton-Exercise/index.js
@@ -1,3 +1,17 @@
 function solve(){
-   // TODO
+   const tBody = document.querySelector('tbody');
+   const tRows = document.querySelectorAll('tbody tr');
+
+   tBody.addEventListener('click', (e) => {
+      const tRow = e.target.parentNode;
+
+      Array.from(tRows).forEach(r => {
+         if (r !== tRow || r.style.cssText) {
+            r.style.cssText = '';
+         }
+         else {
+            r.style.cssText = 'background-color: rgb(65, 63, 94)';
+         }
+      });
+   });
 }
