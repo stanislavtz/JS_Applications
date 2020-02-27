f903ad6ef096529245b76256b07a28a1117f18bf
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-02-27 08:48:55 +0200

    Hex update

--- 01. This/hex.js
+++ 01. This/hex.js
@@ -12,15 +12,25 @@ class Hex {
     }
 
     plus(value) {
-        return this.toString.call(15);
+        let that = this.valueOF() + this.valueOF.call(value);
+        return `0x${that.toString(16).toUpperCase()}`;
+    }
+
+    minus(value) {
+        let that = this.valueOF() - this.valueOF.call(value);
+        return `0x${that.toString(16).toUpperCase()}`;
+    }
+
+    parse(str) {
+        return parseInt(str, 16)
     }
 
 }
-
+let FF = new Hex(255);
+console.log(FF.toString());
+console.log(FF.valueOf() + 1);
+console.log(FF.valueOf() + 1 == 256);
 let a = new Hex(10);
 let b = new Hex(5);
-console.log(a.toString())
-console.log(b.toString())
-console.log(toString.call(15))
-// console.log(a.plus(b).toString());
-
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString()==='0xF');
