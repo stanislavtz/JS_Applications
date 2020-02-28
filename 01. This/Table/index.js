6fb666864d64a084d9e3cd7ef6e245c96d004696
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-02-28 08:54:21 +0200

    Renamed files and Next-Article upload

--- 01. This/Table/index.js
+++ 01. This/Table/index.js
@@ -3,15 +3,14 @@ function solve(){
    const tRows = document.querySelectorAll('tbody tr');
 
    tBody.addEventListener('click', (e) => {
-      const tRow = e.target.parentNode;
+      const tr = e.target.parentNode;
 
       Array.from(tRows).forEach(r => {
-         if (r !== tRow || r.style.cssText) {
+         if (r !== tr) {
             r.style.cssText = '';
          }
-         else {
-            r.style.cssText = 'background-color: rgb(65, 63, 94)';
-         }
       });
+
+      tr.style.cssText = tr.style.cssText ? '' : 'background-color: rgb(65, 63, 94)';
    });
 }
