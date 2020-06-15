f78accb017bdd058d4c71cdc1e53ef964541695e
Tzekov <stanislavtz@gmail.com>
2020-06-15 16:19:35 +0300

    company solution update

--- 01. This/company.js
+++ 01. This/company.js
@@ -1,68 +1,58 @@
 class Company {
     constructor() {
-        this._departments = [];
+        this.departments = [];
     }
 
     addEmployee(username, salary, position, department) {
-        if(!username || !salary || !position || !department){
-            throw new Error("Invalid input!");
+        if (!(username && salary && position && department)) {
+            throw new Error('Invalid input!');
         }
 
-        if(salary < 0) {
-            throw new Error("Invalid input!");
-        }
-        
-        if (!this._departments.map(x => x.name).includes(department)) {
-            this._departments.push({name: department, employees: []})
+        if (Number(salary) <= 0) {
+            throw new Error('Invalid input!');
         }
 
-        let currentDprt = this._departments.find(d => d.name === department);
-        currentDprt.employees.push({name: username, salary: salary, position: position});
-        
+        let current = this.departments.find(d => d.name === department);
+
+        if (!current) {
+            current = {
+                name: department,
+                employees: []
+            }
+
+            this.departments.push(current);
+        }
+
+        const newEmloyee = {
+            username,
+            salary,
+            position
+        }
+
+        current.employees.push(newEmloyee);
         return `New employee is hired. Name: ${username}. Position: ${position}`;
     }
 
     bestDepartment() {
-        const sortedDepatments = this.calculateAverageSalaries(this._departments)
-            .sort((a, b) => {
-                if(a.averageSalary === b.averageSalary){
-                    return a.name.localeCompare(b.name);
-                }
-                
-                return b.averageSalary - a.averageSalary;
-            });
-            
-            const bestDpr = sortedDepatments[0];
-            
-            let result = '';            
-            result += `Best Department is: ${bestDpr.name}\n`;
-            result += `Average salary: ${bestDpr.averageSalary.toFixed(2)}\n`;
-            
-            result += `${bestDpr.employees
-                .sort((a, b) => {
-                    if(a.salary === b.salary) {
-                        return a.name.localeCompare(b.name)
-                    }
-                    
-                    return b.salary - a.salary;
-                })
-                .map(x => `${x.name} ${x.salary} ${x.position}`)
-                .join('\n').trim()
-            }`;
-            
-        return result;
-    }
+        let result = [];
+        this.departments.forEach(d => {
+            const totalDepartmentSalary = d.employees
+                .map(e => e.salary)
+                .reduce((a, b) => +a + +b);
 
-    calculateAverageSalaries(departments) {
-        for (const department of departments) {
-            let averageSalary = department.employees
-                .reduce((acc, empl) => {
-                    acc += empl.salary;
-                    return acc;
-                }, 0) / department.employees.length;
-            department['averageSalary'] = averageSalary;
-        }
+            d.averageSalary = totalDepartmentSalary / d.employees.length;
+            d.employees
+                .sort((a, b) => a.username.localeCompare(b.username))
+                .sort((a, b) => b.salary - a.salary);
+        });
 
-        return departments;
+        const sorted = this.departments.sort((a, b) => b.averageSalary - a.averageSalary);
+        const bestDepartment = sorted[0];
+
+        result.push(`Best Department is: ${bestDepartment.name}`);
+        result.push(`Average salary: ${bestDepartment.averageSalary.toFixed(2)}`);
+        result.push(bestDepartment.employees.map(emp => `${emp.username} ${emp.salary} ${emp.position}`).join('\n'));
+
+        return result.join('\n');
     }
 }
