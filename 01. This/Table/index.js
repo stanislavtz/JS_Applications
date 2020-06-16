f24a7c47349d28bc67ae1a9479bc2f9fb858d8f3
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-06-16 19:53:39 +0300

    correct Table app update

--- 01. This/Table/index.js
+++ 01. This/Table/index.js
@@ -14,20 +14,3 @@ function solve() {
       tr.style.backgroundColor = tr.style.backgroundColor ? '' : '#413f5e';
    }
 }
-
-// function solve(){
-//    const tBody = document.querySelector('tbody');
-//    const tRows = document.querySelectorAll('tbody tr');
-
-//    tBody.addEventListener('click', (e) => {
-//       const tr = e.target.parentNode;
-
-//       Array.from(tRows).forEach(r => {
-//          if (r !== tr) {
-//             r.style.cssText = '';
-//          }
-//       });
-
-//       tr.style.cssText = tr.style.cssText ? '' : 'background-color: rgb(65, 63, 94)';
-//    });
-// }
