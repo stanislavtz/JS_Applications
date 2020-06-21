d5993f5da84f49ba6a88598614e31b0f3d620c5d
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-06-22 00:22:58 +0300

    arrayExtension update

--- 02. Object-Composition/arrayExtension.js
+++ 02. Object-Composition/arrayExtension.js
@@ -1,5 +1,5 @@
 (function () {
-    Array.prototype.last = function () { return this[- 1] };
+    Array.prototype.last = function () { return this[this.length - 1] };
     Array.prototype.skip = function (n) { return this.slice(n) };
     Array.prototype.take = function (n) { return this.slice(0, n) };
     Array.prototype.sum = function () { return this.reduce((a, b) => +a + +b) };
