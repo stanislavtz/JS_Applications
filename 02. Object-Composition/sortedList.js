d65473f085cf58d48b5b57190bdd887fb507c54d
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-06-22 00:14:31 +0300

    Sorted list update

--- 02. Object-Composition/sortedList.js
+++ 02. Object-Composition/sortedList.js
@@ -1,78 +1,36 @@
 function solve() {
-    class List {
-        constructor() {
-            this.list = [];
-        }
+    let list = [];
 
-        get size() {
-            return this.list.length;
-        }
+    function add(el) {
+        list.push(el);
+        list.sort((a, b) => a - b);
+        this.size = list.length;
+    };
 
-        add(el) {
-            this.list.push(el);
-            this.list.sort((a, b) => a - b);
-        }
+    function remove(index) {
+        validateIndex(index);        
+        list.splice(index, 1);
+        this.size = list.length;
+    }
 
-        remove(index) {
-            if (_checkIndex(index)) {
-                this.list.splice(index, 1);
-            }
-        }
+    function get(index) {
+        validateIndex(index);
+        return list[index];
+    }
 
-        get(index) {
-            if (_checkIndex(index)) {
-                return this.list[index];
-            }
-        }
-
-        _checkIndex(index) {
-            if (index >= 0 && index <= this.list.length - 1) {
-                return true;
-            }
-            return false;
+    function validateIndex(index) {
+        if(index >= 0 && index <= list.length - 1) {
+            throw new Error('Index is out of range!')
         }
     }
 
-    return new List()
-    
+    return {
+        add,
+        remove,
+        get,
+        size: 0
+    }
 }
-
-// function result() {
-//     let list = [];
-
-//     function add(el) {
-//         list.push(el);
-//         list.sort((a, b) => +a - +b);
-//         this.size = list.length;
-//     };
-
-//     function remove(index) {
-//         if (checkIndex(index)) {
-//             list.splice(index, 1);
-//             this.size = list.length;
-//         }
-//     }
-
-//     function get(index) {
-//         if (checkIndex(index)) {
-//             return list[index];
-//         }
-//     }
-
-//     function checkIndex(index) {
-//         if(index >= 0 && index <= list.length - 1) {
-//             return true;
-//         }
-//         return false;
-//     }
-
-//     return {
-//         add,
-//         remove,
-//         get,
-//         size: 0
-//     }
-// }
 
 let mylist = solve();
 console.log(mylist.hasOwnProperty('add'))
