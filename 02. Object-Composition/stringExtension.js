62ed541ecf81b9dd34069880867dc7fb73f80d58
Tzekov <stanislavtz@gmail.com>
2020-06-22 10:29:28 +0300

    stringExtension update

--- 02. Object-Composition/stringExtension.js
+++ 02. Object-Composition/stringExtension.js
@@ -39,15 +39,47 @@
     };
 
     String.format = function (input) {
-        for (let i = 0; i < 10; i++) {
-            if (input.includes(`${i}`)) {
+        let i = 0;
+        while(true) {
+            if (input.includes(i)) {
                 input = input.replace(`{${i}}`, `${arguments[i + 1] ? arguments[i + 1] : `{${i}}`}`);
+                i++;
+                continue;
             }
-            else {
-                break;
-            }
+
+            break;
         }
 
         return input.toString();
     }
 })()
+
+let str = 'my string';
+console.log(str);
+
+str = str.ensureStart('my');
+console.log(str);
+
+str = str.ensureStart('hello ');
+console.log(str);
+
+str = str.truncate(16);
+console.log(str);
+
+str = str.truncate(14);
+console.log(str);
+
+str = str.truncate(8);
+console.log(str);
+
+str = str.truncate(4);
+console.log(str);
+
+str = str.truncate(2);
+console.log(str);
+
+str = String.format('The {0} {1} fox', 'quick', 'brown');
+console.log(str);
+
+str = String.format('jumps {0} {1}', 'dog');
+console.log(str);
