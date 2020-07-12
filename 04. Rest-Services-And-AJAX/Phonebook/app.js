08b70e4ecafccb87eaa71a0b3062acd378ea502c
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-07-12 11:02:38 +0300

    Phonebook app update

--- 04. Rest-Services-And-AJAX/Phonebook/app.js
+++ 04. Rest-Services-And-AJAX/Phonebook/app.js
@@ -1,54 +1,51 @@
 import * as data from './data.js';
 import el from './createElement.js';
 
-window.addEventListener('load', () => {
+function attachEvents() {
+    const loadBtn = document.querySelector("#btnLoad");
+    const createBtn = document.querySelector("#btnCreate");
+    const contactsList = document.querySelector('#phonebook');
 
-    function attachEvents() {
-        const loadBtn = document.querySelector("#btnLoad");
-        const createBtn = document.querySelector("#btnCreate");
-        const contactsList = document.querySelector('#phonebook');
-    
-        loadBtn.addEventListener('click', loadContacts);
-        createBtn.addEventListener('click', createContact);
-    
-        async function loadContacts() {
-            contactsList.textContent = '';
-    
-            let contacts = await data.loadContacts();
-    
-            try {
-                Object.entries(contacts).forEach(([contactId, info]) => {
-                    const li = el('li', `${info.person}: ${info.phone}`);
-                    const delBtn = el('button', 'Delete');
-                    delBtn.addEventListener('click', async () => {
-                        await data.deleteContact(contactId);
-                        await loadContacts();
-                    });
-        
-                    li.appendChild(delBtn);
-                    contactsList.appendChild(li);
+    loadBtn.addEventListener('click', loadContacts);
+    createBtn.addEventListener('click', createContact);
+
+    async function loadContacts() {
+        contactsList.textContent = '';
+
+        let contacts = await data.loadContacts();
+
+        try {
+            Object.entries(contacts).forEach(([contactId, info]) => {
+                const li = el('li', `${info.person}: ${info.phone}`);
+                const delBtn = el('button', 'Delete');
+                delBtn.addEventListener('click', async () => {
+                    await data.deleteContact(contactId);
+                    await loadContacts();
                 });
-            } catch (error) {
-                contactsList.textContent = "The phonebook is empty!!!"
-            }
     
+                li.appendChild(delBtn);
+                contactsList.appendChild(li);
+            });
+        } catch (error) {
+            contactsList.textContent = "The phonebook is empty!!!"
         }
-    
-        async function createContact() {
-            const person = document.querySelector('#person');
-            const phone = document.querySelector('#phone');
-            const contact = {
-                person: person.value,
-                phone: phone.value
-            }
-    
-            await data.createContact(contact);
-            await loadContacts();
-    
-            person.value = '';
-            phone.value = '';
-    
-        }
+
     }
-    attachEvents();
-})
+
+    async function createContact() {
+        const person = document.querySelector('#person');
+        const phone = document.querySelector('#phone');
+        const contact = {
+            person: person.value,
+            phone: phone.value
+        }
+
+        await data.createContact(contact);
+        await loadContacts();
+
+        person.value = '';
+        phone.value = '';
+
+    }
+}
+attachEvents();
