4bbcf341257653a4acecef011ade44db182af29c
Stanislavtz Tzekov <stanislavtz@gmail.com>
2020-07-14 00:51:37 +0300

    Remote DB Books app update

--- 06. Remote-Databases/BOOKS/app.js
+++ 06. Remote-Databases/BOOKS/app.js
@@ -1,11 +1,93 @@
-import * as data from'./data.js';
+import * as data from './data.js';
+import el from '../domElCreator.js';
 
-(function solve(){
+(function solve() {
+    const title = document.querySelector('#title');
+    const author = document.querySelector('#author');
+    const isbn = document.querySelector('#isbn');
+
     const loadBtn = document.querySelector('#loadBooks');
     loadBtn.addEventListener('click', loadBooks);
-    
-    function loadBooks() {
-        const books = data.getBooks();
-        console.log(books)
+
+    const submitBtn = document.querySelector('form button');
+    submitBtn.addEventListener('click', addBook);
+
+    async function loadBooks() {
+        const tBody = document.querySelector('tbody');
+        tBody.innerHTML = '<tr><td colspan="4"><h3>Loading...</h3></td></tr>';
+
+        const books = await data.getBooks();
+        tBody.textContent = '';
+
+        books
+            .sort((a, b) => a.title.localeCompare(b.title))
+            .sort((a, b) => a.author.localeCompare(b.author))
+            .map(b => renderBook(b));
+
+        title.value = '';
+        author.value = '';
+        isbn.value = '';
+
+        function renderBook(book) {
+            const tr = el('tr');
+            const tdTilte = el('td', `${book.title}`);
+            const tdAuthor = el('td', `${book.author}`);
+            const tdISBN = el('td', `${book.isbn}`);
+            const tdBTNS = el('td');
+
+            const editBtn = el('button', 'Edit');
+
+            editBtn.addEventListener('click', async () => {
+                const newBook = {
+                    title: title.value,
+                    author: author.value,
+                    isbn: isbn.value
+                };
+                await data.updateBook(book, newBook);
+                loadBooks();
+            });
+
+            const delBtn = el('button', 'Delete');
+            delBtn.addEventListener('click', async () => {
+                await data.deleteBook(book.objectId);
+                loadBooks();
+            });
+
+            tdBTNS.appendChild(editBtn);
+            tdBTNS.appendChild(delBtn);
+
+            tr.appendChild(tdTilte);
+            tr.appendChild(tdAuthor);
+            tr.appendChild(tdISBN);
+            tr.appendChild(tdBTNS);
+
+            return tBody.appendChild(tr);
+        }
+    }
+
+    async function addBook(e) {
+        e.preventDefault();
+        const books = await data.getBooks();
+
+        const book = {
+            title: title.value,
+            author: author.value,
+            isbn: isbn.value
+        };
+
+        if(books.map(b => b.title).includes(book.title)) {
+            title.value = '';
+            title.placeholder='This title exist'
+            return;
+        }
+
+
+        try {
+            await data.addBook(book)
+            loadBooks();
+        } catch (error) {
+            console.error(error);
+        }
+
     }
 })()
--- 06. Remote-Databases/BOOKS/data.js
+++ 06. Remote-Databases/BOOKS/data.js
@@ -6,20 +6,52 @@ function host(endPoint) {
 }
 
 export async function getBooks() {
-    const response = await fetch(host('books'));
-    const data = await response.json()
-    return data;
+    const res = await fetch(host('books'));
+    const body = await res.json()
+    return body;
 }
 
 export async function addBook(book) {
+    for (const key of Object.keys(book)) {
+        if (!book[key]) {
+            document.querySelector(`#${key}`).placeholder = `Place correct ${key}!`;
+            throw new Error(`The ${key} should not be empty!!!`);
+        }
+    }
 
+    const res = await fetch(host('books'), {
+        method: 'POST',
+        body: JSON.stringify(book),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    const body = await res.json();
+    return body;
 }
 
-export async function updateBook(id) {
+export async function updateBook(book, newBook) {
+    Object.keys(newBook).map(key => {
+        if(!newBook[key]){
+            newBook[key] = book[key];
+        }
+    });
 
+    const res = await fetch(host(`books/${book.objectId}`), {
+        method: 'PUT',
+        body: JSON.stringify(newBook),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    return await res.json();
 }
 
 export async function deleteBook(id) {
-
+    const res = await fetch(host(`books/${id}`), {
+        method: 'DELETE'
+    });
+    return res;
 }
-getBooks()
