2e9dade42a655007453d9f62b84f4d36e7e659cb
Tzekov <stanislavtz@gmail.com>
2020-09-11 13:30:31 +0300

    Phonebook application update

--- 04. Rest-Services-And-AJAX/Phonebook/app.js
+++ 04. Rest-Services-And-AJAX/Phonebook/app.js
@@ -1,60 +1,70 @@
 import * as data from './data.js';
-import el from './createElement.js';
+import el from '../cr_el.js';
 
 window.addEventListener('load', attachEvents);
 
 function attachEvents() {
-    const loadBtn = document.querySelector("#btnLoad");
-    const createBtn = document.querySelector("#btnCreate");
-    const contactsList = document.querySelector('#phonebook');
-
+    const loadBtn = document.querySelector('#btnLoad');
     loadBtn.addEventListener('click', loadContacts);
+
+    const createBtn = document.querySelector('#btnCreate');
     createBtn.addEventListener('click', createContact);
+}
 
-    async function loadContacts() {
-        contactsList.textContent = '';
+async function createContact(e) {
+    const personEl = document.querySelector('#person');
+    const phoneEl = document.querySelector('#phone');
 
-        let contacts = Object.entries(await data.loadContacts());
-
-        try {
-            if(contacts.length === 0) { throw new Error()}
-
-            contacts.forEach(([contactId, info]) => {
-                const li = el('li', `${info.person}: ${info.phone}`);
-                const delBtn = el('button', 'Delete');
-                delBtn.addEventListener('click', async (e) => {
-                    e.target.parentNode.remove();
-                    await data.deleteContact(contactId);
-                });
-    
-                li.appendChild(delBtn);
-                contactsList.appendChild(li);
-            });
-        } catch (error) {
-            contactsList.textContent = "The phonebook is empty!!!";
-        }
-
+    if (!personEl.value || !phoneEl.value) {
+        e.target.textContent = 'Please fill corect data and Click ME again';
+        return;
+    } else {
+        e.target.textContent = 'Create';
     }
 
-    async function createContact() {
-        try {
-            const person = document.querySelector('#person');
-            const phone = document.querySelector('#phone');
-            const contact = {
-                person: person.value,
-                phone: phone.value
-            }
-    
-            if(!(contact.person && contact.phone)) {
-                throw new Error()
-            }
-            await data.createContact(contact);
-            loadContacts();
-    
-            person.value = '';
-            phone.value = '';
-        } catch (error) {
-            contactsList.textContent = "Please fill all filds!!!";
-        }
+    const contact = {
+        person: personEl.value,
+        phone: phoneEl.value
     }
+
+    await data.createContact(contact);
+    loadContacts();
+
+    personEl.value = '';
+    phoneEl.value = '';
+}
+
+async function loadContacts() {
+    const contactsEl = document.querySelector('#phonebook');
+    const contacts = await data.loadContacts();
+
+    contactsEl.textContent = '';
+
+    emptyPhonebookNote(contacts, contactsEl);
+    displayContacts(contacts, contactsEl);
+}
+
+function emptyPhonebookNote(contacts, contactsEl) {
+    if (Object.keys(contacts).length === 0) {
+        const h2 = el('h2', 'This phonebook is Empty');
+        contactsEl.appendChild(h2);
+
+        return;
+    }
+}
+
+function displayContacts(contacts, contactsEl) {
+    Object.entries(contacts).forEach(([id, info]) => {
+        const li = el('li', `${info.person}: ${info.phone} `);
+        const delBtn = el('button', 'Delete');
+
+        delBtn.addEventListener('click', async function (e) {
+            e.target.parentNode.remove();
+            await data.deleteContact(id);
+        });
+
+        li.appendChild(delBtn);
+
+        contactsEl.appendChild(li);
+    });
 }
--- 04. Rest-Services-And-AJAX/Phonebook/data.js
+++ 04. Rest-Services-And-AJAX/Phonebook/data.js
@@ -1,9 +1,9 @@
-function getUrl(endPoint) {
-    return `http://localhost:8000/phonebook/${endPoint}`;
+function getUrl(input) {
+    return `http://localhost:8000/` + input;
 }
 
 export function loadContacts() {
-    return fetch(getUrl(``)).then(r => r.json());
+    return fetch(getUrl('phonebook')).then(r => r.json());
 }
 
 export function createContact(contact) {
@@ -12,13 +12,11 @@ export function createContact(contact) {
         body: JSON.stringify(contact)
     };
 
-    return fetch(getUrl(''), obj).then(r => r.json());
+    return fetch(getUrl('phonebook'), obj).then(r => r.json());
 }
 
 export function deleteContact(id) {
-    const obj = {
-        method: "DELETE"
-    };
-    
-    return fetch(getUrl(`${id}`), obj).then(r => r.json());
+    const obj = { method: 'DELETE' };
+
+    return fetch(getUrl(`phonebook/${id}`), obj);
 }
