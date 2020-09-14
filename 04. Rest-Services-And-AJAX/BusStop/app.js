aaca10a7a2889496ce71b8ba4a0ccf72efbe5f80
Tzekov <stanislavtz@gmail.com>
2020-09-14 12:15:26 +0300

    BusStop update

--- 04. Rest-Services-And-AJAX/BusStop/app.js
+++ 04. Rest-Services-And-AJAX/BusStop/app.js
@@ -9,10 +9,13 @@ function getUrl(endPoint) {
 
 async function getInfo() {
     busesListEl.textContent = '';
+
     try {
         let result = await fetch(getUrl(stopIdEl.value)).then(r => r.json());
+        if(!result.name) { throw new Error()}
+
         stopNameEl.textContent = result.name;
-    
+
         for (const key in result.buses) {
             const li = document.createElement('li');
             li.textContent = `Bus ${key} arrives in ${result.buses[key]}`;
