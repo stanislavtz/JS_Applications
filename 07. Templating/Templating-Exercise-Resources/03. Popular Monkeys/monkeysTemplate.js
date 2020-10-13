493f5d488b4c652ad2bddc8b23c166a3aa7c941f
Tzekov <stanislavtz@gmail.com>
2020-10-13 11:56:02 +0300

    Templates exercise updates

--- 07. Templating/Templating-Exercise-Resources/03. Popular Monkeys/monkeysTemplate.js
+++ 07. Templating/Templating-Exercise-Resources/03. Popular Monkeys/monkeysTemplate.js
@@ -2,10 +2,10 @@ import monkeys from "./monkeys.js";
 (async () => {
     Handlebars.registerPartial(
         'monkey',
-        await fetch('./monkeyTemplate.hbs').then(x => x.text())
+        await fetch('./templates/monkeyTemplate.hbs').then(x => x.text())
     );
 
-    const templateFn = Handlebars.compile(await fetch('./allMonkeysTemplate.hbs').then(x => x.text()));
+    const templateFn = Handlebars.compile(await fetch('./templates/allMonkeysTemplate.hbs').then(x => x.text()));
     const htmlToAdd = templateFn({ monkeys });
 
     document.querySelector('section').innerHTML = htmlToAdd;
